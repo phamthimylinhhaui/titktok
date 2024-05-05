c37d16247922e087ef309ef1dc5a454d9751706d
hapo-linhptm <linh.ptm@haposoft.com>
2024-05-06 02:04:19 +0700

    lesson-49: random gÃift

--- src/App.js
+++ src/App.js
@@ -2,47 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react'
 
-const order = [100, 2, 500];
+const gifts = [
+  'pt 1',
+  'pt 2',
+  'pt 3',
+];
 function App() {
-  const [counter, setCounter] = useState(
-    order.reduce((total, curr) => (total + curr))
-  );
-  const [info, setInfo] = useState({
-    name: 'linh',
-    age: '12',
-  });
-  const handleIncrease = () => {
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    // setCounter(counter + 1);
-    // how to setCounter + 3 and re-render 1 time =>use callback in setState
+  const [gift, setGift] = useState()
 
-    setCounter(pre => pre + 1)
-    setCounter(pre => pre + 1)
-    setCounter(pre => pre + 1)
-  }
-
-  const handleAddInfo = () => {
-    setInfo({
-      ...info,
-      bio: 'yeu mau hong'
-    })
+  const randomGift = () => {
+    const index = Math.floor(Math.random() * gifts.length)
+    setGift(gifts[index]);
   }
 
   return (
     <div className="App">
       <div>
-        <h1>add info</h1>
-        <p>{JSON.stringify(info)}</p>
-        <button onClick={handleAddInfo}>
-          add information
-        </button>
-      </div>
-      <div>
-        <h1>counter</h1>
-        <p>{counter}</p>
-        <button onClick={handleIncrease}>
-          increase
+        <h1>{gift || 'chua co phan thuong'}</h1>
+
+        <button onClick={randomGift}>
+          lay thuong
         </button>
       </div>
     </div>
