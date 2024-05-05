724ce266ac0a40310b5f12a07676d94a44330075
hapo-linhptm <linh.ptm@haposoft.com>
2024-05-06 01:51:49 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
+const order = [100, 2, 500];
 function App() {
+  const [counter, setCounter] = useState(
+    order.reduce((total, curr) => (total + curr))
+  );
+  const [info, setInfo] = useState({
+    name: 'linh',
+    age: '12',
+  });
+  const handleIncrease = () => {
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // how to setCounter + 3 and re-render 1 time =>use callback in setState
+
+    setCounter(pre => pre + 1)
+    setCounter(pre => pre + 1)
+    setCounter(pre => pre + 1)
+  }
+
+  const handleAddInfo = () => {
+    setInfo({
+      ...info,
+      bio: 'yeu mau hong'
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>add info</h1>
+        <p>{JSON.stringify(info)}</p>
+        <button onClick={handleAddInfo}>
+          add information
+        </button>
+      </div>
+      <div>
+        <h1>counter</h1>
+        <p>{counter}</p>
+        <button onClick={handleIncrease}>
+          increase
+        </button>
+      </div>
     </div>
   );
 }
