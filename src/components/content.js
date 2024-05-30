0601735593dbfa09571e0d8903f38419b3c5e295
hapo-linhptm <linh.ptm@haposoft.com>
2024-05-31 06:01:59 +0700

    lesson-52: useeffect des

--- src/components/content.js
+++ src/components/content.js
@@ -1,26 +1,36 @@
 import { useEffect, useState } from 'react'
 
+const tabs = ['posts', 'comments', 'albums']
 export default function Content()
 {
   const [title, setTitle] = useState('');
   const [data, setData] = useState([]);
+  const [type, setType] = useState('posts');
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then(res => res.json())
       .then(posts => setData(posts))
-    console.log('call in effect')
-  }, [])
+  }, [type])
 
 
   return (<div>
     <div>
+      {tabs.map(tab => (
+        <button
+          key={tab}
+          onClick={() => setType(tab)}
+          style={tab === type ? {color:'white', background:'black'} : {}}
+        >
+          {tab}
+      </button>))}
+    </div>
+    <div>
       <label htmlFor="title">input title</label><br/>
       <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
     </div>
-    {console.log('re-render UI')}
     <div>
       <ul>
-        {data.map(post => (<li key={post.id}>{post.title}</li>))}
+        {data.map(post => (<li key={post.id}>{post.title ?? post.name}</li>))}
       </ul>
     </div>
   </div>)
